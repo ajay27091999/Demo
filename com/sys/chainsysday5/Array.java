ec366ad25e4b252007792cace24f8797fa033f44
ajay3349 <ajay3349@CSLH3143.global.chainsys.com>
2022-11-17 17:34:30 +0530

    Array

--- com/sys/chainsysday5/Array.java
+++ com/sys/chainsysday5/Array.java
@@ -3,7 +3,18 @@ package com.sys.chainsysday5;
 public class Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] name=new String[10];
+		
+		name[0]="Ajay";
+		name[1]="Prag";
+		name[2]="riya";
+		name[3]="kumar";
+		name[4]="Raja";
+		
+		//System.out.println(name[10]);
+	for(int i=0;i<name.length;i++) {
+		System.out.println(name[i]);
+	}
 
 	}
 
