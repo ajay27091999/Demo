a5dca78ab6422c2395f789ed0972c406f189cc23
ajay3349 <ajay3349@CSLH3143.global.chainsys.com>
2022-11-17 17:40:18 +0530

    Employee Err

---
+++ com/sys/chainsysday5/Emp.java
@@ -0,0 +1,16 @@
+package com.sys.chainsysday5;
+
+public class Emp {
+
+	String name;
+	int id;
+	String designation;
+	
+	
+	Emp(String name,int id,	String designation){
+		this.name=name;
+		this.id=id;
+		this.designation=designation;
+	}
+
+}
