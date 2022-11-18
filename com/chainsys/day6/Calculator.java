70550a25bc07bd36b7305a755fab8d7ce457132f
ajay3349 <ajay3349@CSLH3143.global.chainsys.com>
2022-11-18 16:59:17 +0530

    Methods day6

---
+++ com/chainsys/day6/Calculator.java
@@ -0,0 +1,18 @@
+package com.chainsys.day6;
+
+public class Calculator {
+
+	int num1,num2;
+	
+	public void add() {
+		System.out.println(num1+num2);
+	}
+	
+	public void sub() {
+		System.out.println(num1-num2);
+	}
+	public void div() {
+		System.out.println(num1/num2);
+	}
+	
+}
