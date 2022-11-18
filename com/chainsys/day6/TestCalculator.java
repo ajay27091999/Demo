39c2562def8c0323974c116bf20120a8e57204d5
ajay3349 <ajay3349@CSLH3143.global.chainsys.com>
2022-11-18 17:01:40 +0530

    Methods TestCal

---
+++ com/chainsys/day6/TestCalculator.java
@@ -0,0 +1,18 @@
+package com.chainsys.day6;
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Calculator c1=new Calculator();
+        c1.num1=20;
+        c1.num2=30;
+        c1.add();
+        c1.sub();
+        c1.div();
+     
+
+
+	}
+
+}
