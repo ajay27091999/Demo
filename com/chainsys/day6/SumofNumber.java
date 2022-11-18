be7a61e08e68cdfe9cd1fed6b842a7c86fde95c3
ajay3349 <ajay3349@CSLH3143.global.chainsys.com>
2022-11-18 16:55:14 +0530

    Assign Sumofnumber

---
+++ com/chainsys/day6/SumofNumber.java
@@ -0,0 +1,22 @@
+package com.chainsys.day6;
+
+import java.util.Scanner;
+
+public class SumofNumber {
+
+	public static void main(String[] args) {
+		int number, digit, sum = 0;  
+		Scanner sc = new Scanner(System.in);  
+		System.out.print("Enter the number: ");  
+		number = sc.nextInt();  
+		while(number > 0)  
+		{    
+		digit = number % 10;  
+		sum = sum + digit;  
+		number = number / 10;  
+		}  
+	 
+		System.out.println("Sum of Number: "+sum);  
+	}
+
+}
