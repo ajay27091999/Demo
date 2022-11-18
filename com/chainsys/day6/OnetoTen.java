4eaf365fd3d75c54a3012f36061350fab343c3d9
ajay3349 <ajay3349@CSLH3143.global.chainsys.com>
2022-11-18 16:52:50 +0530

    Assign one to ten

---
+++ com/chainsys/day6/OnetoTen.java
@@ -0,0 +1,15 @@
+package com.chainsys.day6;
+
+public class OnetoTen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		System.out.println("The numbers print from 1 to 10");
+		while (i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
