0ad6298ac532284c8f731ce72f888cbadda70cbc
ajay3349 <ajay3349@CSLH3143.global.chainsys.com>
2022-11-18 16:47:09 +0530

    Assign Add

---
+++ com/chainsys/day6/Addition.java
@@ -0,0 +1,20 @@
+package com.chainsys.day6;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		        int a = 4;
+		        a++;
+		        a += a;
+		        --a;
+		        a = 7 + a;
+		        a *= a;
+		        a -= 3;
+
+		        System.out.println(" a = " + a );
+	
+	}
+
+}
